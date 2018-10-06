74fba867bc253beb9a96396c05fdc38a26c481ee
KrystianJonca <33162450+KrystianJonca@users.noreply.github.com>
2018-10-06 11:57:04 +0200

    Create first usefull functions
    Create first usefull functions

--- src/helpers.js
+++ src/helpers.js
@@ -1,6 +1,28 @@
+import chalk from 'chalk'
 // import os from 'os'
 
 export default {
+  // Main function
+  async createMessage (program) {
+    let msg = chalk`{bold.cyan Here are the informations what you asked for:}`
+
+    if (program.spec) msg += await this.pcSpec()
+    if (program.cpu) msg += await this.cpuUsage()
+    if (program.memory) msg += await this.memoryUsage()
+    if (program.disk) msg += await this.diskUsage()
+
+    if (!program.spec && !program.cpu && !program.memory && !program.disk) {
+      msg += await this.pcSpec()
+      msg += await this.cpuUsage()
+      msg += await this.memoryUsage()
+      msg += await this.diskUsage()
+
+      return msg
+    }
+
+    return msg
+  },
+  // Helpers
   async pcSpec () { },
   async cpuUsage () { },
   async memoryUsage () { },
--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('Work')
+import program from 'commander'
+import helpers from './helpers'
+import { version } from '../package'
+
+program
+  .version(version, '-v, -version')
+  .option('-s, -spec', 'Display PC Specification')
+  .option('-c, -cpu', 'Display CPU Usage')
+  .option('-m, -memory', 'Display Memory Usage')
+  .option('-d, -disk', 'Display Disk Usage')
+  .parse(process.argv)
+
+helpers.createMessage(program).then(msg => {
+  console.log(msg)
+})
+
+export default program
