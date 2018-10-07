301b3f9f1b0fabe94174ad8d1dd6ef3c25456ef6
KrystianJonca <33162450+KrystianJonca@users.noreply.github.com>
2018-10-07 20:18:21 +0200

    Add first usefull functions

--- src/helpers.js
+++ src/helpers.js
@@ -1,14 +1,15 @@
+import si from 'systeminformation'
 import chalk from 'chalk'
 // import os from 'os'
 
 export default {
   // Main function
   async createMessage (program) {
-    let msg = chalk`{bold.cyan Here are the informations what you asked for:}`
+    let msg = chalk`{bold.cyan Here are the informations what you asked for:}\n`
 
     if (program.spec) msg += await this.pcSpec()
     if (program.cpu) msg += await this.cpuUsage()
-    if (program.memory) msg += await this.memoryUsage()
+    if (program.cemory) msg += await this.memoryUsage()
     if (program.disk) msg += await this.diskUsage()
 
     if (!program.spec && !program.cpu && !program.memory && !program.disk) {
@@ -23,8 +24,23 @@ export default {
     return msg
   },
   // Helpers
-  async pcSpec () { },
-  async cpuUsage () { },
-  async memoryUsage () { },
-  async diskUsage () { }
+  async pcSpec () {
+    const os = await si.osInfo()
+    const cpu = await si.cpu()
+    const moba = await si.baseboard()
+    const gpu = await si.graphics()
+
+    const msg = chalk`{bold.green OS:} ${os.distro} ${os.arch}\n{bold.green CPU:} ${cpu.manufacturer} ${cpu.brand} (${cpu.speed}Ghz ${cpu.cores} Cores)\n{bold.green GPU:} ${gpu.controllers[0].model}(${gpu.controllers[0].vram} VRAM)\n{bold.green MOBA:} ${moba.manufacturer} ${moba.model}`
+
+    return msg
+  },
+  async cpuUsage () {
+    return 'Coming soon!'
+  },
+  async memoryUsage () {
+    return 'Coming soon!'
+  },
+  async diskUsage () {
+    return 'Coming soon!'
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import program from 'commander'
+import chalk from 'chalk'
 import helpers from './helpers'
 import { version } from '../package'
 
 program
   .version(version, '-v, -version')
-  .option('-s, -spec', 'Display PC Specification')
-  .option('-c, -cpu', 'Display CPU Usage')
-  .option('-m, -memory', 'Display Memory Usage')
-  .option('-d, -disk', 'Display Disk Usage')
+  .option('-s, --spec', 'Display PC Specification')
+  .option('-c, --cpu', 'Display CPU Usage')
+  .option('-m, --memory', 'Display Memory Usage')
+  .option('-d, --disk', 'Display Disk Usage')
   .parse(process.argv)
 
 helpers.createMessage(program).then(msg => {
   console.log(msg)
+}).catch(err => {
+  console.log(chalk`{bold.red Something went wrong! :( \n ${err}}`)
+  process.exit(1)
 })
 
 export default program
