a3a5200ae564b7f55c7e437337d6115c71817172
KrystianJonca <33162450+KrystianJonca@users.noreply.github.com>
2018-10-09 17:27:59 +0200

    Add battery information function
    Add battery information function

--- src/helpers.js
+++ src/helpers.js
@@ -11,12 +11,14 @@ export default {
     if (program.cpu) msg += await this.cpuUsage()
     if (program.cemory) msg += await this.memoryUsage()
     if (program.disk) msg += await this.diskUsage()
+    if (program.battery) msg += await this.battery()
 
     if (!program.spec && !program.cpu && !program.memory && !program.disk) {
       msg += await this.pcSpec()
       msg += await this.cpuUsage()
       msg += await this.memoryUsage()
       msg += await this.diskUsage()
+      msg += await this.battery()
 
       return msg
     }
@@ -29,18 +31,35 @@ export default {
     const cpu = await si.cpu()
     const moba = await si.baseboard()
     const gpu = await si.graphics()
+    const ram = await si.mem()
 
-    const msg = chalk`{bold.green OS:} ${os.distro} ${os.arch}\n{bold.green CPU:} ${cpu.manufacturer} ${cpu.brand} (${cpu.speed}Ghz ${cpu.cores} Cores)\n{bold.green GPU:} ${gpu.controllers[0].model}(${gpu.controllers[0].vram} VRAM)\n{bold.green MOBA:} ${moba.manufacturer} ${moba.model}`
+    const msg = chalk`{bold.green ---- PC SPEC ----}\n{bold.green OS:} ${os.distro} ${os.arch}\n{bold.green CPU:} ${cpu.manufacturer} ${cpu.brand} (${cpu.speed}Ghz ${cpu.cores} Cores)\n{bold.green GPU:} ${gpu.controllers[0].model}(${gpu.controllers[0].vram} VRAM)\n{bold.green MOBA:} ${moba.manufacturer} ${moba.model}\n{bold.green RAM:} ${this.bytesToGb(ram.total)}\n`
 
     return msg
   },
   async cpuUsage () {
-    return 'Coming soon!'
+    const msg = chalk`{bold.green ---- CPU USAGE ----}\n`
+
+    return msg
   },
   async memoryUsage () {
-    return 'Coming soon!'
+    const msg = chalk`{bold.green ---- MEMORY USAGE ----}\n`
+
+    return msg
   },
   async diskUsage () {
-    return 'Coming soon!'
+    const msg = chalk`{bold.green ---- DISK USAGE ----}\n`
+
+    return msg
+  },
+  async battery () {
+    const msg = chalk`{bold.green ---- BATTERY ----}\n`
+
+    return msg
+  },
+  bytesToGb (bytes) {
+    if (bytes === 0) return '0 Byte'
+    let i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)))
+    return `${Math.round(bytes / Math.pow(1024, i), 2)} GB`
   }
 }
